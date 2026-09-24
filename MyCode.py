a7f94e90348fcbc0c0417e9bc04f426db3f8b9e6
Hamza-Ziani-Dev <hamzaziani.dev@gmail.com>
2026-09-24 15:25:11 +0100

    SC-12 Fix PyScript fetch/create_proxy errors so Add/Subtract buttons work

--- MyCode.py
+++ MyCode.py
@@ -6,3 +6,8 @@ def add_numbers(a, b):
 def sub_numbers(a, b):
     return a - b
 
+def get_user_input():
+    a = float(input("first number:"))
+    b = float(input("second number:"))
+    return a, b
+
